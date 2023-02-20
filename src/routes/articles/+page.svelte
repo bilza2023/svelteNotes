8828c3e65986873cc1589b3fbb245a36cf7d095a
unknown <bilza32611@gmail.com>
2023-02-20 16:06:40 +0500

    todays targets if achieved then there will be no coding after that - today edit and delte

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -2,6 +2,7 @@
 import Table from "./table.svelte";
 import {enhance} from "$app/forms";
 export let  data;
+let filterToggle = false;
 const articlesAll = [...data.articles];
 // export let form;
 let articles = articlesAll;
@@ -13,17 +14,26 @@ let searchText = "";
 // console.log(data);
 // }
 function search( ) {
-articles = articlesAll; // reinstate all the remove items
-
-  const filteredArticles = articles.filter(article => {
   const trimmed = searchText.trim();
-  const lower  = trimmed.toLocaleLowerCase(); 
+  if (trimmed == ""){return;}
+const lower  = trimmed.toLocaleLowerCase(); 
+
+filterToggle = true;
+articles = articlesAll; // reinstate all the remove items
+  
+  const filteredArticles = articles.filter(article => {
     return article.title.toLowerCase().includes( lower);
   });
 //   console.log(filteredArticles);
 articles = filteredArticles;
 }
 
+
+function removeFilter(){
+filterToggle = false;
+searchText = "";
+articles = [...articlesAll];
+}
 </script>
 
 
@@ -36,9 +46,21 @@ class="bg-blue-600 flex justify-center items-center py-2"
  <!-- search bar form -->
 <input name = "searchText" bind:value={searchText}
 class="px-2 py-1 border rounded-l" type="text">
+
+{#if filterToggle}
+<button class="px-4 py-1 m-1 bg-green-800 text-white rounded-r"
+on:click={removeFilter}
+>X</button>
+{/if}
+
+
 <button class="px-4 py-1 bg-blue-800 text-white rounded-r"
 on:click|preventDefault = {search}
 >Search</button>
+
+<div class="bg-blue-800 text-white text-xl p-1 m-1">
+Found : {articles.length}
+</div>
 
 </div>
 <hr/>
---
+++ src/routes/articles/targets.md
@@ -0,0 +1,19 @@
+
+
+Targets for Search Page
+
+# Stage 1 : Its alive (today)
+1. Remove filter
+2. total number of items shown
+3. edit (view is already made)
+3. categories: filter by category
+
+# stage 2 : Improvement is business as usual (tomarrow).
+4. for list get titles only not content
+4. pagination
+    . tags //--may not be required go directly to phase 3
+ 
+//============ new project
+
+# stage 3 : Taxonomy System
+5. xxxxx
